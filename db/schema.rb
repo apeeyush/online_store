ffe2e503e1e44ebefed9cdc43aa53859ef67508e
peeyush <peeyushagarwal1994@gmail.com>
2014-05-08 20:29:37 +0530

    Add Travis Support

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220073456) do
+ActiveRecord::Schema.define(version: 20121130000009) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
